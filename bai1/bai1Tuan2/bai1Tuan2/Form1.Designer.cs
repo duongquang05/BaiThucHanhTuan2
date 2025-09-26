5113e96ab146f7ab3c650b66dec84e416b976292
duongquang05 <vanquangg05@gmail.com>
2025-09-26 22:25:24 +0700

    Add files via upload

---
+++ bai1/bai1Tuan2/bai1Tuan2/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace bai1Tuan2
+{
+    partial class FormDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDangNhap = new System.Windows.Forms.Label();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.txtDangNhap = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnDung = new System.Windows.Forms.Button();
+            this.chkNho = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblDangNhap
+            // 
+            this.lblDangNhap.AutoSize = true;
+            this.lblDangNhap.BackColor = System.Drawing.SystemColors.Control;
+            this.lblDangNhap.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDangNhap.Location = new System.Drawing.Point(62, 86);
+            this.lblDangNhap.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDangNhap.Name = "lblDangNhap";
+            this.lblDangNhap.Size = new System.Drawing.Size(151, 26);
+            this.lblDangNhap.TabIndex = 0;
+            this.lblDangNhap.Text = "Tên đăng nhập";
+            this.lblDangNhap.Click += new System.EventHandler(this.lblDangNhap_Click);
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.BackColor = System.Drawing.SystemColors.Control;
+            this.lblMatKhau.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatKhau.Location = new System.Drawing.Point(62, 163);
+            this.lblMatKhau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(103, 26);
+            this.lblMatKhau.TabIndex = 1;
+            this.lblMatKhau.Text = "Mật Khẩu";
+            this.lblMatKhau.Click += new System.EventHandler(this.lblMatKhau_Click);
+            // 
+            // txtDangNhap
+            // 
+            this.txtDangNhap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtDangNhap.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtDangNhap.Location = new System.Drawing.Point(262, 86);
+            this.txtDangNhap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDangNhap.Name = "txtDangNhap";
+            this.txtDangNhap.Size = new System.Drawing.Size(289, 33);
+            this.txtDangNhap.TabIndex = 2;
+            this.txtDangNhap.TextChanged += new System.EventHandler(this.txtDangNhap_TextChanged);
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.BackColor = System.Drawing.SystemColors.Info;
+            this.txtMatKhau.Location = new System.Drawing.Point(262, 163);
+            this.txtMatKhau.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(289, 33);
+            this.txtMatKhau.TabIndex = 3;
+            this.txtMatKhau.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.AutoSize = true;
+            this.btnDangNhap.BackColor = System.Drawing.SystemColors.Control;
+            this.btnDangNhap.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Location = new System.Drawing.Point(43, 284);
+            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(170, 39);
+            this.btnDangNhap.TabIndex = 4;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = false;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.AutoSize = true;
+            this.btnXoa.BackColor = System.Drawing.SystemColors.Control;
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(288, 287);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(112, 39);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xóa ";
+            this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnDung
+            // 
+            this.btnDung.AutoSize = true;
+            this.btnDung.BackColor = System.Drawing.SystemColors.Control;
+            this.btnDung.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDung.Location = new System.Drawing.Point(475, 287);
+            this.btnDung.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDung.Name = "btnDung";
+            this.btnDung.Size = new System.Drawing.Size(112, 39);
+            this.btnDung.TabIndex = 6;
+            this.btnDung.Text = "Dừng";
+            this.btnDung.UseVisualStyleBackColor = false;
+            this.btnDung.Click += new System.EventHandler(this.btnDung_Click);
+            // 
+            // chkNho
+            // 
+            this.chkNho.AutoSize = true;
+            this.chkNho.BackColor = System.Drawing.SystemColors.Control;
+            this.chkNho.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkNho.Location = new System.Drawing.Point(67, 226);
+            this.chkNho.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chkNho.Name = "chkNho";
+            this.chkNho.Size = new System.Drawing.Size(114, 30);
+            this.chkNho.TabIndex = 7;
+            this.chkNho.Text = "Ghi Nhớ";
+            this.chkNho.UseVisualStyleBackColor = false;
+            this.chkNho.CheckedChanged += new System.EventHandler(this.chkNho_CheckedChanged);
+            // 
+            // FormDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(646, 373);
+            this.Controls.Add(this.chkNho);
+            this.Controls.Add(this.btnDung);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtDangNhap);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.lblDangNhap);
+            this.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormDangNhap";
+            this.Text = "Form Đăng Nhập";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDangNhap;
+        private System.Windows.Forms.Label lblMatKhau;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnDung;
+        private System.Windows.Forms.CheckBox chkNho;
+        private System.Windows.Forms.TextBox txtDangNhap;
+    }
+}
+
---
+++ bai1/bai1Tuan2/bai1Tuan2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai1Tuan2
+{
+    public partial class FormDangNhap : Form
+    {
+        public FormDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMatKhau_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDangNhap_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkNho_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string thongbao;
+            thongbao = "ten dang nhap la:";
+            thongbao += this.txtDangNhap.Text;
+            thongbao += "\n mat khau la:";
+            thongbao += this.txtMatKhau.Text;
+            if(this.chkNho.Checked)
+                thongbao += "\n Ban da chon nho mat khau";
+            else
+                thongbao += "\n Ban khong chon nho mat khau";
+            MessageBox.Show(thongbao, "Thong bao");
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            this.txtDangNhap.Clear();
+            this.txtMatKhau.Clear();
+            this.txtDangNhap.Focus();
+        }
+
+        private void btnDung_Click(object sender, EventArgs e)
+        {
+                this.Close();
+        }
+
+        private void txtDangNhap_TextChanged(object sender, EventArgs e)
+        {
+              
+        }
+    }
+}
